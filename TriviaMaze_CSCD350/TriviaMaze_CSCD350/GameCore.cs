e45721e99400d003af9f3ef29809f17b84990bc6
Nathaniel Graham <nrgraham23@gmail.com>
2014-11-03 23:32:52 -0800

    Room display work
    Modified the room display so we can load images into the correct frames.

---
+++ TriviaMaze_CSCD350/TriviaMaze_CSCD350/GameCore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaMaze_CSCD350 {
+    class GameCore {
+        
+        public void SetLDoorImage(String fileName) {
+            
+        }
+
+        public void SetMDoorImage(String fileName) {
+
+        }
+
+        public void SetRDoorImage(String fileName) {
+
+        }
+    }
+}
--- TriviaMaze_CSCD350/TriviaMaze_CSCD350/MainWindow.xaml.cs
+++ TriviaMaze_CSCD350/TriviaMaze_CSCD350/MainWindow.xaml.cs
@@ -13,59 +13,51 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TriviaMaze_CSCD350
-{
+namespace TriviaMaze_CSCD350 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+        private GameCore gameCore;
+
+        public MainWindow() {
             InitializeComponent();
+            this.gameCore = new GameCore();
         }
 
-        private void newGameMenuItemClick(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void saveGameMenuItemClick(object sender, RoutedEventArgs e)
-        {
+        private void newGameMenuItemClick(object sender, RoutedEventArgs e) {
 
         }
 
-        private void loadGameMenuItemClick(object sender, RoutedEventArgs e)
-        {
+        private void saveGameMenuItemClick(object sender, RoutedEventArgs e) {
 
         }
 
-        private void exitMenuItemClick(object sender, RoutedEventArgs e)
-        {
+        private void loadGameMenuItemClick(object sender, RoutedEventArgs e) {
 
         }
 
-        private void aboutGameMenuItemClick(object sender, RoutedEventArgs e)
-        {
+        private void exitMenuItemClick(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void aboutGameMenuItemClick(object sender, RoutedEventArgs e) {
             MessageBox.Show("Welcome to the Trivia maze!\n\nDeveloped by the Twenty Hats team!\nVersion 1.0", "About");
         }
 
-        private void controlsGameMenuItemClick(object sender, RoutedEventArgs e)
-        {
+        private void controlsGameMenuItemClick(object sender, RoutedEventArgs e) {
             MessageBox.Show("The bottom left hand corner you will see a map of\n" +
                             "the maze. \n\nUse it to find your way from where you entered \n" +
                             "(insert color here) to the exit (insert color here)\n" +
                             "by clicking or the doorways to move either forward,\n" +
-                            "backward, left, or right." , "Controls");
+                            "backward, left, or right.", "Controls");
         }
 
-        private void addQuestionGameMenuItemClick(object sender, RoutedEventArgs e)
-        {
+        private void addQuestionGameMenuItemClick(object sender, RoutedEventArgs e) {
 
         }
 
-        private void viewQuestionsGameMenuItemClick(object sender, RoutedEventArgs e)
-        {
+        private void viewQuestionsGameMenuItemClick(object sender, RoutedEventArgs e) {
 
         }
     }
