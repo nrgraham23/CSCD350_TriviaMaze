4b60a68b1ef4d566d0c240e921b7733e9389014a
kymojo <kylesworkshopp@hotmail.com>
2014-11-19 20:44:37 -0800

    Question and QuestionDatabase
    Added Question and QuestionDatabase classes. I removed
    MultiAnswerQuestion, ShortAnswerQuestion, and TFAnswerQuestion (because
    Question class isn't abstract or an interface) as well as TestQuestion
    and RandomQuestionFactory (same functionality in QuestionDatabase).

---
+++ TriviaMaze_CSCD350/TriviaMaze_CSCD350/Question.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriviaMaze_CSCD350 {
+    class Question {
+
+        enum QType { SHORT = 1, TF, MULTI };
+        enum QAux { TEXT = 1, IMAGE, SOUND };
+
+        String[] questionChoices;
+        String questionText, questionAuxFile;
+        int questionAnswer, questionType, questionAuxiliary;
+
+        //=====================================================================
+        // initialize values for default question object
+        public Question() {
+            questionChoices = new String[4] { "Correct", "Wrong", "Nope", "False" };
+            questionText = "Default question";
+            questionAuxFile = "NULL";
+            questionAnswer = 1;
+            questionType = 3;
+            questionAuxiliary = 1;
+        }
+
+        //=====================================================================
+
+        public void SetText(String text) {
+            questionText = text;
+        }
+
+        public void SetAnswer(int answer) {
+            questionAnswer = answer;
+        }
+
+        public void SetQType(int type) {
+            questionType = type;
+        }
+
+        public void SetAuxiliary(int aux) {
+            questionAuxiliary = aux;
+        }
+
+        public void SetAuxFile(String path) {
+            questionAuxFile = path;
+        }
+
+        public void SetChoices(String[] choices) {
+            for (int i = 0; i < 4; i++)
+                questionChoices[i] = choices[i];
+        }
+
+        public void SetChoice(int index, String choice) {
+            if (index >= 1 && index <= 4)
+                questionChoices[index] = choice;
+        }
+
+        //=====================================================================
+
+        public String GetText() {
+            return questionText;
+        }
+
+        public int GetAnswer() {
+            return questionAnswer;
+        }
+
+        public int GetQType() {
+            return questionType;
+        }
+
+        public int GetAuxiliary() {
+            return questionAuxiliary;
+        }
+
+        public String GetAuxFile() {
+            return questionAuxFile;
+        }
+
+        public String[] GetChoices() {
+            return questionChoices;
+        }
+
+        public String GetChoice(int index) {
+            if (index >= 1 && index <= 4)
+                return questionChoices[index];
+            return "";
+        }
+
+        //=====================================================================
+
+        public bool CorrectShortAnswer(String answer) {
+            if (answer.ToLower().Equals(GetChoice(1).ToLower()))
+                return true;
+            return false;
+        }
+
+        public bool CorrectTFAnswer(bool answer) {
+            if ((answer && GetAnswer() == 1) || (!answer && GetAnswer() == 2))
+                return true;
+            return false;
+        }
+
+        public bool CorrectMultiAnswer(int answer) {
+            if (answer == GetAnswer())
+                return true;
+            return false;
+        }
+
+        //=====================================================================
+    }
+}
---
+++ TriviaMaze_CSCD350/TriviaMaze_CSCD350/QuestionDatabase.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace TriviaMaze_CSCD350 {
+    class QuestionDatabase {
+
+        /* [DATABASE SETUP]
+         * Questions: QIndex SMALLINT, QType SMALLINT, QAuxiliary SMALLINT, QAuxFile VARCHAR(255), QText VARCHAR(255),
+         * QAnswer SMALLINT, QOption1 VARCHAR(64), QOption2 VARCHAR(64), QOption3 VARCHAR(64), QOption4 VARCHAR(64)
+         */
+
+        SQLiteConnection dbConnection;
+        ArrayList dbIgnore = new ArrayList();
+        string dbPath;
+        int dbEntries;
+        Random rand = new Random();
+
+        //=====================================================================
+        // Create the database object, open it, and count entries
+        public QuestionDatabase(string dbPath) {
+            this.dbPath = dbPath;
+            if (OpenDatabase())
+                CountEntries();
+        }
+
+        //=====================================================================
+        // Create a new database connection, open it, and test the connection (returns bool type for success flag)
+        public bool OpenDatabase() {
+            dbConnection = new SQLiteConnection("Data Source=" + dbPath + ";Version=3;");
+            dbConnection.Open();
+
+            try { // Try a simple SQL command to check for open success
+                string sql = "SELECT QIndex FROM Questions WHERE QIndex = 1";
+                SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+                SQLiteDataReader reader = command.ExecuteReader();
+            }
+            catch (SQLiteException) {
+                return false;
+            }
+            return true;
+        }
+
+        //=====================================================================
+
+        public void CloseDatabase() {
+            dbConnection.Close();
+        }
+
+        //=====================================================================
+
+        public Question RandomQuestion() {
+
+            int index = -1;
+            do {
+                index = rand.Next(dbEntries) + 1;
+            } while (!ValidIndex(index));
+
+            //dbIgnore.Add(index);
+
+            return GetQuestion(index);
+        }
+
+        //=====================================================================
+        // Grabs a question object from the database and returns it.
+        public Question GetQuestion(int index) {
+
+            if (!HasQuestions() || !ValidIndex(index))
+                return null;
+
+            string sql = "SELECT * FROM Questions WHERE QIndex = " + index;
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            reader.Read();
+
+            Question tempQuestion = new Question();
+
+            tempQuestion.SetQType(Convert.ToInt32("" + reader["QType"]));
+            tempQuestion.SetAuxiliary(Convert.ToInt32("" + reader["QAuxiliary"]));
+            tempQuestion.SetAuxFile("" + reader["QType"]);
+            tempQuestion.SetText("" + reader["QText"]);
+            tempQuestion.SetAnswer(Convert.ToInt32(reader["QAnswer"]));
+            tempQuestion.SetChoices(new String[4] { "" + reader["QOption1"], "" + reader["QOption2"], "" + reader["QOption3"], "" + reader["QOption4"] });
+
+            return tempQuestion;
+        }
+
+        //=====================================================================
+
+        public void AddQuestionToDatabase(Question q) {
+            int index = dbEntries + 1;
+            String sql = "INSERT INTO Questions (QIndex,QType,QAuxiliary,QAuxFile,QText,QAnswer,QOption1,QOption2,QOption3,QOption4) " +
+                         "VALUES (" + index + ", " + q.GetQType() + ", " + q.GetAuxiliary() + ", \"" + q.GetAuxFile() + "\", \"" +
+                         q.GetText() + "\", " + q.GetAnswer() + ", \"" + q.GetChoices()[0] + "\", \"" + q.GetChoices()[1] + "\", \"" +
+                         q.GetChoices()[2] + "\", \"" + q.GetChoices()[3] + "\")";
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+            command.ExecuteNonQuery();
+            dbEntries++;
+        }
+
+        //=====================================================================
+
+        public void DeleteQuestionFromDatabase(int index) {
+            if (index < 1 || index > dbEntries)
+                return;
+            String sql = "DELETE FROM Questions WHERE QIndex = " + index;
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+            command.ExecuteNonQuery();
+            if (index != dbEntries) {
+                sql = "UPDATE Questions SET QIndex = " + index + " WHERE QIndex = " + dbEntries;
+                command = new SQLiteCommand(sql, dbConnection);
+                command.ExecuteNonQuery();
+            }
+            dbEntries--;
+        }
+
+        //=====================================================================
+
+        private bool HasQuestions() {
+            if (dbIgnore.Count == dbEntries) {
+                Console.WriteLine("No more questions!");
+                return false;
+            }
+            return true;
+        }
+
+        //=====================================================================
+
+        private bool ValidIndex(int index) {
+            if (dbIgnore.IndexOf(index) == -1)
+                return true;
+            return false;
+        }
+
+        //=====================================================================
+
+        private void CountEntries() {
+            int entryCount = 0;
+            string sql = "SELECT * FROM Questions";
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+            SQLiteDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+                entryCount++;
+            dbEntries = entryCount;
+        }
+
+        //=====================================================================
+
+        private void UnignoreAll() {
+            dbIgnore.Clear();
+        }
+
+        //=====================================================================
+    }
+}
